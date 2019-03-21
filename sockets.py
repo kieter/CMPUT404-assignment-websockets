fa757af1006b65fc02c73959d78880e60a64dfd6
kieter <balisnom@ualberta.ca>
2019-03-21 14:10:23 -0600

    Finished backend I think? Tests run.

--- sockets.py
+++ sockets.py
@@ -26,6 +26,22 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+def send_all(message):
+    for client in clients:
+        client.put(message)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+    def put(self, v):
+        self.queue.put_nowait(v)
+    def get(self):
+        return self.queue.get()
+
+clients = []
+
 class World:
     def __init__(self):
         self.clear()
@@ -65,7 +81,9 @@ def set_listener( entity, data ):
     ''' do something with the update ! '''
 
 myWorld.add_set_listener( set_listener )
-        
+
+#WS = WEB SOCKET, worst naming ever.
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
@@ -74,14 +92,38 @@ def hello():
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
+    try:
+        while True:
+            message = ws.receive()
+            print("WS RECV: %s" % message)
+            if (message is not None):
+                packet = json.loads(message)
+                send_all_json( packet )
+            else:
+                break
+    except:
+        '''Done'''
+
     return None
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
+       
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )   
+    try:
+        while True:
+            message = client.get()
+            ws.send(message)
+    except Exception as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
